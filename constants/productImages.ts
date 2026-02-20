36be8e83763d1344fe4b44b7f6e6e798aefe7585
ankitsingh2026 <ankitsingh626522@gmail.com>
2026-02-20 16:48:01 +0530

    image uploaded on s3"

--- constants/productImages.ts
+++ constants/productImages.ts
@@ -1,86 +1,89 @@
 // constants/productImages.ts
- 
+
+const S3_BASE =
+  "https://drydash-app-images.s3.ap-south-1.amazonaws.com/rider-images/washrzimages/";
+
 export const productImages: Record<string, any> = {
   /* ===================== LAUNDRY ===================== */
-  "w_f_wearable.jpg": require("@/assets/washrzimages/w_f_wearable.jpg"),
-  "w_f_non-wearable.jpg": require("@/assets/washrzimages/w_f_non-wearable.jpg"),
-  "w_i_wearable.jpg": require("@/assets/washrzimages/w_i_wearable.jpg"),
-  "w_i_non-wearable.jpg": require("@/assets/washrzimages/w_i_non-wearable.jpg"),
- 
+  "w_f_wearable.jpg": { uri: `${S3_BASE}w_f_wearable.jpg` },
+  "w_f_non-wearable.jpg": { uri: `${S3_BASE}w_f_non-wearable.jpg` },
+  "w_i_wearable.jpg": { uri: `${S3_BASE}w_i_wearable.jpg` },
+  "w_i_non-wearable.jpg": { uri: `${S3_BASE}w_i_non-wearable.jpg` },
+
   /* ===================== DRY CLEAN – CLOTHES ===================== */
-  "shirt.png": require("@/assets/washrzimages/shirt.png"),
-  "jeans.png": require("@/assets/washrzimages/jeans.png"),
-  "trouser.png": require("@/assets/washrzimages/trouser.png"),
-  "blazer.png": require("@/assets/washrzimages/blazer.png"),
-  "3_pc_suit.png": require("@/assets/washrzimages/3_pc_suit.png"),
-  "2_pc_suit.png": require("@/assets/washrzimages/2_pc_suit.png"),
-  "longblazer.png": require("@/assets/washrzimages/longblazer.png"),
-  "hoodie.png": require("@/assets/washrzimages/hoodie.png"),
-  "winter_jacket.jpg": require("@/assets/washrzimages/winter_jacket.jpg"),
- 
-  "heavysaree.png": require("@/assets/washrzimages/heavysaree.png"),
-  "mediumsaree.png": require("@/assets/washrzimages/mediumsaree.png"),
-  "saree.png": require("@/assets/washrzimages/saree.png"),
-  "blouse.png": require("@/assets/washrzimages/blouse.png"),
-  "heavy_blouse.webp": require("@/assets/washrzimages/heavy_blouse.webp"),
- 
-  "lehenga.png": require("@/assets/washrzimages/lehenga.png"),
-  "mediumlehenga.png": require("@/assets/washrzimages/mediumlehenga.png"),
-  "heavy_lehenga.jpg": require("@/assets/washrzimages/heavy_lehenga.jpg"),
- 
-  "dress.png": require("@/assets/washrzimages/dress.png"),
-  "heavy_dress.jpg": require("@/assets/washrzimages/heavy_dress.jpg"),
-  "gown.jpg": require("@/assets/washrzimages/gown.jpg"),
-  "heavy_gown.jpg": require("@/assets/washrzimages/heavy_gown.jpg"),
- 
-  "dupatta.jpg": require("@/assets/washrzimages/dupatta.jpg"),
-  "heavy_duptta.jpg": require("@/assets/washrzimages/heavy_duptta.jpg"),
- 
-  "kurta_pajama.jpg": require("@/assets/washrzimages/kurta_pajama.jpg"),
-  "shwal.jpg": require("@/assets/washrzimages/shwal.jpg"),
-  "cardigin.jpg": require("@/assets/washrzimages/cardigin.jpg"),
-  "srug.jpg": require("@/assets/washrzimages/srug.jpg"),
- 
-  "leather_jacket.jpg": require("@/assets/washrzimages/leather_jacket.jpg"),
-  "belt.jpg": require("@/assets/washrzimages/belt.jpg"),
-  "leather_belt.webp": require("@/assets/washrzimages/leather_belt.webp"),
- 
+  "shirt.png": { uri: `${S3_BASE}shirt.png` },
+  "jeans.png": { uri: `${S3_BASE}jeans.png` },
+  "trouser.png": { uri: `${S3_BASE}trouser.png` },
+  "blazer.png": { uri: `${S3_BASE}blazer.png` },
+  "3_pc_suit.png": { uri: `${S3_BASE}3_pc_suit.png` },
+  "2_pc_suit.png": { uri: `${S3_BASE}2_pc_suit.png` },
+  "longblazer.png": { uri: `${S3_BASE}longblazer.png` },
+  "hoodie.png": { uri: `${S3_BASE}hoodie.png` },
+  "winter_jacket.jpg": { uri: `${S3_BASE}winter_jacket.jpg` },
+
+  "heavysaree.png": { uri: `${S3_BASE}heavysaree.png` },
+  "mediumsaree.png": { uri: `${S3_BASE}mediumsaree.png` },
+  "saree.png": { uri: `${S3_BASE}saree.png` },
+  "blouse.png": { uri: `${S3_BASE}blouse.png` },
+  "heavy_blouse.webp": { uri: `${S3_BASE}heavy_blouse.webp` },
+
+  "lehenga.png": { uri: `${S3_BASE}lehenga.png` },
+  "mediumlehenga.png": { uri: `${S3_BASE}mediumlehenga.png` },
+  "heavy_lehenga.jpg": { uri: `${S3_BASE}heavy_lehenga.jpg` },
+
+  "dress.png": { uri: `${S3_BASE}dress.png` },
+  "heavy_dress.jpg": { uri: `${S3_BASE}heavy_dress.jpg` },
+  "gown.jpg": { uri: `${S3_BASE}gown.jpg` },
+  "heavy_gown.jpg": { uri: `${S3_BASE}heavy_gown.jpg` },
+
+  "dupatta.jpg": { uri: `${S3_BASE}dupatta.jpg` },
+  "heavy_duptta.jpg": { uri: `${S3_BASE}heavy_duptta.jpg` },
+
+  "kurta_pajama.jpg": { uri: `${S3_BASE}kurta_pajama.jpg` },
+  "shwal.jpg": { uri: `${S3_BASE}shwal.jpg` },
+  "cardigin.jpg": { uri: `${S3_BASE}cardigin.jpg` },
+  "srug.jpg": { uri: `${S3_BASE}srug.jpg` },
+
+  "leather_jacket.jpg": { uri: `${S3_BASE}leather_jacket.jpg` },
+  "belt.jpg": { uri: `${S3_BASE}belt.jpg` },
+  "leather_belt.webp": { uri: `${S3_BASE}leather_belt.webp` },
+
   /* ===================== DRY CLEAN – BEDDING ===================== */
-  "pillow_cover.jpg": require("@/assets/washrzimages/pillow_cover.jpg"),
-  "large_pillow.jpg": require("@/assets/washrzimages/large_pillow.jpg"),
-  "small_pillow.jpg": require("@/assets/washrzimages/small_pillow.jpg"),
- 
-  "blanket(single).jpg": require("@/assets/washrzimages/blanket(single).jpg"),
-  "double_blanket.jpg": require("@/assets/washrzimages/double_blanket.jpg"),
- 
-  "Druvet_single.jpg": require("@/assets/washrzimages/Druvet_single.jpg"),
-  "Druvet_double.jpg": require("@/assets/washrzimages/Druvet_double.jpg"),
- 
-  "Quilt_single.jpg": require("@/assets/washrzimages/Quilt_single.jpg"),
-  "Quilt_double.jpg": require("@/assets/washrzimages/Quilt_double.jpg"),
- 
-  "bed_cover_single.jpg": require("@/assets/washrzimages/bed_cover_single.jpg"),
-  "bed_cover_double.jpg": require("@/assets/washrzimages/bed_cover_double.jpg"),
-  "bed_sheet_single.jpg": require("@/assets/washrzimages/bed_sheet_single.jpg"),
-  "bed_sheet_double.jpg": require("@/assets/washrzimages/bed_sheet_double.jpg"),
- 
+  "pillow_cover.jpg": { uri: `${S3_BASE}pillow_cover.jpg` },
+  "large_pillow.jpg": { uri: `${S3_BASE}large_pillow.jpg` },
+  "small_pillow.jpg": { uri: `${S3_BASE}small_pillow.jpg` },
+
+  "blanket(single).jpg": { uri: `${S3_BASE}blanket(single).jpg` },
+  "double_blanket.jpg": { uri: `${S3_BASE}double_blanket.jpg` },
+
+  "Druvet_single.jpg": { uri: `${S3_BASE}Druvet_single.jpg` },
+  "Druvet_double.jpg": { uri: `${S3_BASE}Druvet_double.jpg` },
+
+  "Quilt_single.jpg": { uri: `${S3_BASE}Quilt_single.jpg` },
+  "Quilt_double.jpg": { uri: `${S3_BASE}Quilt_double.jpg` },
+
+  "bed_cover_single.jpg": { uri: `${S3_BASE}bed_cover_single.jpg` },
+  "bed_cover_double.jpg": { uri: `${S3_BASE}bed_cover_double.jpg` },
+  "bed_sheet_single.jpg": { uri: `${S3_BASE}bed_sheet_single.jpg` },
+  "bed_sheet_double.jpg": { uri: `${S3_BASE}bed_sheet_double.jpg` },
+
   /* ===================== DRY CLEAN – BAGS ===================== */
-  "handbag_small.jpeg": require("@/assets/washrzimages/handbag_small.jpeg"),
-  "handbag_medium.jpg": require("@/assets/washrzimages/handbag_medium.jpg"),
-  "handbag_large.jpg": require("@/assets/washrzimages/handbag_large.jpg"),
-  "sports_bag.jpg": require("@/assets/washrzimages/sports_bag.jpg"),
-  "Leatherbag_small.webp": require("@/assets/washrzimages/Leatherbag_small.webp"),
-  "leather_bag_large.jpg": require("@/assets/washrzimages/leather_bag_large.jpg"),
- 
+  "handbag_small.jpeg": { uri: `${S3_BASE}handbag_small.jpeg` },
+  "handbag_medium.jpg": { uri: `${S3_BASE}handbag_medium.jpg` },
+  "handbag_large.jpg": { uri: `${S3_BASE}handbag_large.jpg` },
+  "sports_bag.jpg": { uri: `${S3_BASE}sports_bag.jpg` },
+  "Leatherbag_small.webp": { uri: `${S3_BASE}Leatherbag_small.webp` },
+  "leather_bag_large.jpg": { uri: `${S3_BASE}leather_bag_large.jpg` },
+
   /* ===================== SHOE SPA ===================== */
-  "sportsshoes.png": require("@/assets/washrzimages/sportsshoes.png"),
-  "leather_shoes.jpg": require("@/assets/washrzimages/leather_shoes.jpg"),
-  "suedeshoes.png": require("@/assets/washrzimages/suedeshoes.png"),
-  "boots.png": require("@/assets/washrzimages/boots.png"),
-  "stilettos.png": require("@/assets/washrzimages/stilettos.png"),
-  "sliders.png": require("@/assets/washrzimages/sliders.png"),
-  "sandals.png": require("@/assets/washrzimages/sandals.png"),
- 
+  "sportsshoes.png": { uri: `${S3_BASE}sportsshoes.png` },
+  "leather_shoes.jpg": { uri: `${S3_BASE}leather_shoes.jpg` },
+  "suedeshoes.png": { uri: `${S3_BASE}suedeshoes.png` },
+  "boots.png": { uri: `${S3_BASE}boots.png` },
+  "stilettos.png": { uri: `${S3_BASE}stilettos.png` },
+  "sliders.png": { uri: `${S3_BASE}sliders.png` },
+  "sandals.png": { uri: `${S3_BASE}sandals.png` },
+
   /* ===================== FALLBACK ===================== */
-  fallback: require("@/assets/washrzimages/sportsshoes.png"),
+  fallback: { uri: `${S3_BASE}sportsshoes.png` },
 };
